cb1616e180f057b4b08da63dad110ef2087adb16
diogomene <diogodelimamenezes15@gmail.com>
2023-08-30 22:48:52 -0400

    medidor de tempo

--- experimentosUtil.py
+++ experimentosUtil.py
@@ -1,4 +1,21 @@
 import random
+import time
+from ordenacaoUtil import *
+
+def medirTempo(metodo, vetor, ini = None, fim = None):
+    vetorCopia = vetor.copy()
+    params = []
+
+    if(ini == None and fim == None):
+        params = [vetorCopia]
+    else:
+        params = [vetorCopia, ini, fim]
+    
+    inicioT = time.time()
+    metodo(*params)
+    fimT = time.time()
+    return fimT - inicioT
+
 def geraVetor(tam):
     vetor = []
     for _ in range(1, tam+1):
@@ -7,9 +24,12 @@ def geraVetor(tam):
 
 
 def iniciar(inc,fim,stp,rpt):
+    resultados = []
     tamanhosVetores = range(inc, fim+1, stp)
-    for tam in tamanhosVetores:
-        vetor = geraVetor(tam)
-        print(vetor)
-
-iniciar(10, 100, 10, 1)
+    for _ in range(rpt):
+        for tam in tamanhosVetores:
+            resultadoVetor = [tam]
+            vetor = geraVetor(tam)
+            resultadoVetor.append(medirTempo(mergeSort, vetor, 0, len(vetor)-1))
+            print(resultadoVetor)
+iniciar(10, 10000, 1000, 2)
